e17d091e536036298f11975aef5d29ff5dff9c0f
diphendara <cristiancvacas@gmail.com>
2018-05-20 23:43:23 +0200

    change the enums and add the first scraping script test

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,9 @@
 class Card < ApplicationRecord
-    enum type: [:Attack, :Skill, :Power, :Status, :Curse]
-    enum deck: [:Green, :Red, :Blue, :Colorless]
-    has_and_belongs_to_many :keywords
+  enum type: {"Attack" => 0, "Skill" => 1, "Power" => 2, "Status" => 3, "Curse" => 4}
+  enum type: {"Green" => 0, "Red" => 1, "Blue" => 2, "Colorless" => 3}
 
-     #validations
-     validates_presence_of :energyCost, :type, :deck
+  has_and_belongs_to_many :keywords
+
+  #validations
+  validates_presence_of :energyCost, :type, :deck
 end
--- app/models/relic.rb
+++ app/models/relic.rb
@@ -1,7 +1,7 @@
 class Relic < ApplicationRecord
-    enum rarity: [:Starter, :Common, :Uncommon, :Rare, :Boss, :Event, :Shop]
-    has_and_belongs_to_many :keywords
+  enum rarity: {"Starter" => 0, "Common" => 1, "Uncommon" => 2, "Rare" => 3, "Boss" => 4, "Event" => 6, "Shop" => 6}
+  has_and_belongs_to_many :keywords
 
-    # validations
-    validates_presence_of :name, :rarity, :description, :lore
+  # validations
+  validates_presence_of :name, :rarity, :description, :lore
 end
